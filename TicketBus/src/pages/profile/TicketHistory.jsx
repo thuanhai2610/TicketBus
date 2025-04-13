6bb2cb2631e65beb02498a87a29e716dd66c591c
thuanhai2 <thuanhaidz22@gmail.com>
2025-04-13 14:35:41 +0700

    getTicket

--- TicketBus/src/pages/profile/TicketHistory.jsx
+++ TicketBus/src/pages/profile/TicketHistory.jsx
@@ -4,21 +4,41 @@ import axios from "axios";
 
 const TicketHistory = () => {
     const [tickets, setTickets] = useState([]);
-    
+    const username = localStorage.getItem("username");
 
+    const formatDateTime = (dateString) => {
+        if (!dateString) return 'N/A';
+        
+        const date = new Date(dateString);
+        if (isNaN(date.getTime())) return 'Invalid Date';
+        
+        return date.toLocaleString('vi-VN', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+      };
+      const formatVND = (value) => {
+        return new Intl.NumberFormat("vi-VN", {
+          style: "currency",
+          currency: "VND",
+        }).format(value);
+      };
     useEffect(() => {
         fetchTickets();
     }, []);
 
     const fetchTickets = async () => {
         try {
-            const response = await axios.get("http://localhost:3001/tickets");
+            const username = localStorage.getItem("username");
+            const response = await axios.get(`http://localhost:3001/tickets/user/${username}`);
             setTickets(response.data);
         } catch (error) {
             console.error("Error fetching tickets:", error);
         }
     };
-
 
     return (
         <div className="p-6">
@@ -33,6 +53,11 @@ const TicketHistory = () => {
                 >
                     Đặt vé
                 </Link>
+                <button 
+                    className="bg-red-600  hover:bg-primaryblue dark:hover:text-neutral-800 hover:text-neutral-950 text-white px-6 py-2 rounded-full shadow transition"
+                    >
+                  Hủy vé
+                </button>
             </div>
 
          
@@ -52,11 +77,11 @@ const TicketHistory = () => {
                         {tickets.length > 0 ? (
                             tickets.map((ticket, idx) => (
                                 <tr key={idx} className="text-sm border-t hover:bg-gray-50 dark:text-neutral-50">
-                                    <td className="p-3 border">{ticket.code}</td>
-                                    <td className="p-3 border">{ticket.route}</td>
-                                    <td className="p-3 border">{ticket.date}</td>
-                                    <td className="p-3 border">{ticket.amount} đ</td>
-                                    <td className="p-3 border">{ticket.status}</td>
+                                    <td className="p-3 border">{ticket._doc.ticketId}</td>
+                                    <td className="p-3 border">{ticket.departurePoint} → {ticket.destinationPoint}</td>
+                                    <td className="p-3 border">{formatDateTime(ticket.departureTime)}</td>
+                                    <td className="p-3 border" >{formatVND(ticket._doc.ticketPrice)}</td>
+                                    <td className="p-3 border">{ticket._doc.status}</td>
                                 </tr>
                             ))
                         ) : (
@@ -68,7 +93,7 @@ const TicketHistory = () => {
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3v18h18V3H3z" />
                                         </svg>
                                     </div>
-                                    No Data
+                                    Bạn chưa đặt vé nào.
                                 </td>
                             </tr>
                         )}
