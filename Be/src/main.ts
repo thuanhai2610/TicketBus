8a7b57113682a06071c7e6760c1ab4085e0e07bf
thuanhai2 <thuanhaidz22@gmail.com>
2025-05-08 19:50:00 +0700

    UpdateMap

--- Be/src/main.ts
+++ Be/src/main.ts
@@ -5,39 +5,26 @@ import * as express from 'express';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import { ExpressAdapter } from '@nestjs/platform-express';
-
+import { Request, Response } from 'express';
 dotenv.config();
-
 async function bootstrap() {
-  const expressApp = express(); // Express gốc
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-
+  const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3001;
-
   app.enableCors({
     origin: process.env.FE,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-
-  expressApp.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
-  expressApp.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
   app.useGlobalPipes(new ValidationPipe());
+  app.use(express.static(join(__dirname, '..', 'dist/public')));
 
-  // Static file cho frontend (Vite build)
-  expressApp.use(express.static(join(__dirname, '..', 'dist', 'public')));
+  // Route fallback for frontend (SPA)
 
-  // Fallback route: trả về index.html cho tất cả các route frontend
-  expressApp.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '..', 'dist', 'public', 'index.html'));
-  });
-
-  await app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  await app.listen(PORT, () => { console.log(` Server running on port ${PORT}`);});
 }
-
 bootstrap();
